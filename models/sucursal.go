48af982f84134cba0e0eacba83371c1d324d9a58
jjvargass <jotavargas89@gmail.com>
2018-01-11 14:38:40 -0500

    relatesell for core api sucursal

--- models/sucursal.go
+++ models/sucursal.go
@@ -47,7 +47,7 @@ func GetSucursalById(id int) (v *Sucursal, err error) {
 func GetAllSucursal(query map[string]string, fields []string, sortby []string, order []string,
 	offset int64, limit int64) (ml []interface{}, err error) {
 	o := orm.NewOrm()
-	qs := o.QueryTable(new(Sucursal))
+	qs := o.QueryTable(new(Sucursal)).RelatedSel()
 	// query k=v
 	for k, v := range query {
 		// rewrite dot-notation to Object__Attribute
