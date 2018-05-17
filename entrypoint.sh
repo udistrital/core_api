aa793a74c01e69c8d6e16e2eb06012d3f54a542f
Fabio Parra <faparraf@gmail.com>
2018-05-17 16:04:37 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ set -u
 set -o pipefail
 
 if [ -n "${PARAMETER_STORE:-}" ]; then
-  export CORE_API__PGUSER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/core_api/db/username --output text --query Parameter.Value)"
-  export CORE_API__PGPASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/core_api/db/password --output text --query Parameter.Value)"
+  export CORE_API_DB_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/core_api/db/username --output text --query Parameter.Value)"
+  export CORE_API_DB_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/core_api/db/password --output text --query Parameter.Value)"
 fi
 
 exec ./main "$@"
